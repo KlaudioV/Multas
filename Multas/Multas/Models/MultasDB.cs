764f6f35f4154ad67447e0863b02e6acbd087b8f
Claudio Jose Baptista Velez <aluno18367@ipt.pt>
2019-04-02 17:01:08 +0100

    Criação inicial da BD

---
+++ Multas/Multas/Models/MultasDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class MultasDB : DbContext
+    {
+        //Identificar  qual o SGBD a usar
+
+
+        //Definir as tabelas da BD
+        public DbSet<Agentes> Agentes{ get; set; }
+        public DbSet<Condutores> Condutores { get; set; }
+        public DbSet<Multas> Multas { get; set; }
+        public DbSet<Viaturas> Viaturas { get; set; }
+    }
+}
