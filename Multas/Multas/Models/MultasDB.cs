b4e1ea6566c29eaec6f29d69cbf4584bbed3a331
Claudio Jose Baptista Velez <aluno18367@ipt.pt>
2019-04-09 16:44:17 +0100

    Adição de restrição ao modelo para impedir a criação das chaves 'on delete cascade' nas FK's

--- Multas/Multas/Models/MultasDB.cs
+++ Multas/Multas/Models/MultasDB.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -12,9 +13,19 @@ namespace Multas.Models
 
 
         //Definir as tabelas da BD
-        public DbSet<Agentes> Agentes{ get; set; }
+        public DbSet<Agentes> Agentes { get; set; }
         public DbSet<Condutores> Condutores { get; set; }
         public DbSet<Multas> Multas { get; set; }
         public DbSet<Viaturas> Viaturas { get; set; }
+
+        //Método a ser executado no início da criação do Modelo
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Eliminar a convenção de atribuir automáticamente o 'on Delete Cascade' nas FK's
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
+
